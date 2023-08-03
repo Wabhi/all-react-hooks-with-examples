99e4e0858330f031eba529afc8696668841af51b
Wabhi <tiwariabhishek27611@gmail.com>
2023-08-04 04:30:45 +0530

    implemented-useRef-hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import UseRef from './UseRef';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <UseRef />
     </div>
   );
 }
