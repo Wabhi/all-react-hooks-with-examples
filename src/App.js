31a2fba8a5c78d2d2417c5bb8a4c56b9bec559bd
Wabhi <tiwariabhishek27611@gmail.com>
2023-08-04 03:16:07 +0530

    implemented useeffect hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import UseState from './Hooks/UseState';
+import UseEffect from './Hooks/UseEffect';
 
 export default function App() {
   return (
     <div>
-      <UseState />
+      {/* <UseState /> */}
+      <UseEffect />
     </div>
   );
 }
